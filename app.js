8498e4ec4657a2d465b3b9edc74f207f9016b7ba
Sourish Roy <sourish.roy1973@gmail.com>
2025-03-20 17:30:52 +0530

    Project + MongoDB Setup

---
+++ app.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
+const app = express();
+const connectDB = require("./db/connect.js");
+
+// error handler
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.use(express.json());
+// extra packages
+
+// routes
+app.get("/", (req, res) => {
+  res.send("jobs api");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
