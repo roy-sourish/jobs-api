a82c016bb3dbac1bdf2c3bf35ff1e8aebbe680d5
Sourish Roy <sourish.roy1973@gmail.com>
2025-03-24 10:48:58 +0000

    security packages and custom errors

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 require("dotenv").config();
 require("express-async-errors");
+
+// extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 const connectDB = require("./db/connect.js");
 
 const authRouter = require("./routes/auth.js");
 const jobsRouter = require("./routes/jobs.js");
-const authenticateUser = require("./middleware/authentication.js")
+const authenticateUser = require("./middleware/authentication.js");
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-app.use(express.json());
 // extra packages
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 mins
+    max: 100, // limit each ip to 100 req per windowMs
+  })
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
